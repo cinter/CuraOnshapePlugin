377bbcc498825838b68a7a5bed74f01d565b624b
Erwan MATHIEU <wawanbreton@gmail.com>
2026-09-06 11:06:52 +0200

    Keep the same dialog over re-opening
    This is useful for 2 reasons:
    * The default storage is not updated (could be, but that avoid it)
    * It is actually convenient when leaving Cura open, change a few
    settings on Onshape, and re-download the same part (or set of parts) a
    few times in a row, which is a common workflow

--- src/OnshapeFileProvider.py
+++ src/OnshapeFileProvider.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2023 Erwan MATHIEU
 
-from typing import TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
 
 import os
 
@@ -17,6 +17,7 @@ from .api.OnshapeApi import OnshapeApi
 
 if TYPE_CHECKING:
     from cura.CuraApplication import CuraApplication
+    from PyQt6.QtCore import QObject
 
 
 class OnshapeFileProvider(FileProvider):
@@ -42,6 +43,8 @@ class OnshapeFileProvider(FileProvider):
             Logger.warning("OnShape Plugin translation not loaded")
 
         application.getPreferences().addPreference("plugin_onshape/tesselation_resolution", "moderate")
+        application.getPreferences().addPreference("plugin_onshape/default_storage_name", "")
+        application.getPreferences().addPreference("plugin_onshape/default_storage_url", "")
 
         self._application: "CuraApplication" = application
         self._auth_controller: OAuthController = OAuthController(self._application)
@@ -52,12 +55,15 @@ class OnshapeFileProvider(FileProvider):
         self._auth_controller.tokenChanged.connect(self._onTokenChanged)
         self._controller.partSelected.connect(self._onPartSelected)
 
+        self._dialog: Optional['QObject'] = None
+
     def run(self) -> None:
         """Main entry point called by the application when the users asks for opening Onshape"""
-        plugin_path = os.path.dirname(os.path.dirname(__file__))
-        dialog_path = os.path.join(plugin_path, 'resources', 'qml', 'MainDialog.qml')
-        self._dialog = self._application.createQmlComponent(dialog_path,
-                                                            {'controller': self._controller})
+        if self._dialog is None:
+            plugin_path = os.path.dirname(os.path.dirname(__file__))
+            dialog_path = os.path.join(plugin_path, 'resources', 'qml', 'MainDialog.qml')
+            self._dialog = self._application.createQmlComponent(dialog_path, {'controller': self._controller})
+
         self._dialog.show()
 
     @pyqtSlot(str)
